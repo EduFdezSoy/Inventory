9fdb8050a4954834a576f8ed32ea7e06959c9d20
Damián Cabello <jdamiancabello@gmail.com>
2018-10-04 11:59:05 +0200

    Añadida pantalla de inicio y de login.

---
+++ app/src/main/java/com/example/usuario/inventory/SplashActivity.java
@@ -0,0 +1,18 @@
+package com.example.usuario.inventory;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * Clase que muestras la imagen del proyecto y que se conectará al repositorio de los datos.
+ */
+
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
