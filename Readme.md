c9708c9ccebaad7efb55aea1409045aa9121f94d
Damián Cabello <jdamiancabello@gmail.com>
2018-10-05 10:52:16 +0200

    Varias modificaciones, mirar Readme para más detalles.

--- Readme.md
+++ Readme.md
@@ -1,7 +1,22 @@
 Log:
 
-4/Octubre/2018
+**4/Octubre/2018**
 
     Inicio de la App
-        -Creacion de panata de inicio.
+        -Creacion de pantalla de inicio.
         -Creacion de pantalla de login.
+
+**5/Octubre/2018**
+    - Modificacion de estilos de la App
+        -TextView del login.
+        -EditText del login.
+        -Botones del login.
+        -Creacion de background_shape_edittex.xml
+
+    Creacion de hilos
+        -Añadido un hilo que se crea mientras dura el logo.
+        -Añadido un finalizador para el logo (para evitar que vuelva atrás).
+
+    Refactorizaciones
+        -Refactorizado el Thread.Sleep y el Runnable (Se obvia uno) gracias al método postDelayed() de [Handler](https://developer.android.com/reference/android/os/Handler).
+        -Quitado los paddings de styles.xml pues mueve los items después de crearlos.
--- app/src/main/java/com/example/usuario/inventory/SplashActivity.java
+++ app/src/main/java/com/example/usuario/inventory/SplashActivity.java
@@ -1,5 +1,7 @@
 package com.example.usuario.inventory;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -10,9 +12,53 @@ import android.os.Bundle;
 
 public class SplashActivity extends AppCompatActivity {
 
+    private final long TIEMPODEESPERA = 2000;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        final Handler uiHandler = new Handler();
+
+        /**
+         * Este objeto se ejecutará en el hilo de la UI
+         */
+        final Runnable onUi = new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this,LoginActivity.class);
+                SplashActivity.this.startActivity(intent);
+                SplashActivity.this.finish();
+            }
+        };
+
+        uiHandler.postDelayed(onUi,TIEMPODEESPERA);
+
+        /**
+         * Este proceso se ejecuta en un hilo diferente de la IU
+         * y manda un mensaje al manejador (Handler)
+         * esta comentado por que no es demasiado efectivo
+         */
+//        Runnable background = new Runnable() {
+//            @Override
+//            public void run() {
+
+//                Esta es la implementación un poco más tosca que la que hacemos más abajo.
+//                try {
+//                    Thread.sleep(TIEMPODEESPERA);
+//                    uiHandler.post(onUi);
+//                } catch (InterruptedException e) {
+//                    e.printStackTrace();
+//                }
+//                uiHandler.postDelayed(onUi,TIEMPODEESPERA);
+//            }
+//        };
+//        new Thread(background).start();
     }
 }
